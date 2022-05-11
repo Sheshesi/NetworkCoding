679e64b3a4f789e5d4f5ee9b6ba75bfe5d7daf01
Sheshesi <alexkadyrbayev@gmail.com>
2022-05-12 00:36:45 +0700

    Create course work

---
+++ SimpleHTTPServer/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace SimpleHTTPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startServerBtn = new System.Windows.Forms.Button();
+            this.stopServerBtn = new System.Windows.Forms.Button();
+            this.serverLogsText = new System.Windows.Forms.TextBox();
+            this.serverPortText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startServerBtn
+            // 
+            this.startServerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startServerBtn.Location = new System.Drawing.Point(351, 405);
+            this.startServerBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.startServerBtn.Name = "startServerBtn";
+            this.startServerBtn.Size = new System.Drawing.Size(90, 26);
+            this.startServerBtn.TabIndex = 0;
+            this.startServerBtn.Text = "Start Server";
+            this.startServerBtn.UseVisualStyleBackColor = true;
+            this.startServerBtn.Click += new System.EventHandler(this.startServerBtn_Click);
+            // 
+            // stopServerBtn
+            // 
+            this.stopServerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopServerBtn.Location = new System.Drawing.Point(351, 435);
+            this.stopServerBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.stopServerBtn.Name = "stopServerBtn";
+            this.stopServerBtn.Size = new System.Drawing.Size(90, 26);
+            this.stopServerBtn.TabIndex = 1;
+            this.stopServerBtn.Text = "Stop Server";
+            this.stopServerBtn.UseVisualStyleBackColor = true;
+            this.stopServerBtn.Click += new System.EventHandler(this.stopServerBtn_Click);
+            // 
+            // serverLogsText
+            // 
+            this.serverLogsText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serverLogsText.Location = new System.Drawing.Point(11, 28);
+            this.serverLogsText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.serverLogsText.Multiline = true;
+            this.serverLogsText.Name = "serverLogsText";
+            this.serverLogsText.ReadOnly = true;
+            this.serverLogsText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.serverLogsText.Size = new System.Drawing.Size(766, 373);
+            this.serverLogsText.TabIndex = 2;
+            // 
+            // serverPortText
+            // 
+            this.serverPortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serverPortText.Location = new System.Drawing.Point(685, 438);
+            this.serverPortText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.serverPortText.Name = "serverPortText";
+            this.serverPortText.Size = new System.Drawing.Size(92, 23);
+            this.serverPortText.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(682, 414);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Server Logs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 473);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serverPortText);
+            this.Controls.Add(this.serverLogsText);
+            this.Controls.Add(this.stopServerBtn);
+            this.Controls.Add(this.startServerBtn);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Simple HTTP Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startServerBtn;
+        private System.Windows.Forms.Button stopServerBtn;
+        private System.Windows.Forms.TextBox serverLogsText;
+        private System.Windows.Forms.TextBox serverPortText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SimpleHTTPServer/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SimpleHTTPServer
+{
+    public partial class Form1 : Form
+    {
+
+        private Socket httpServer;
+        private int serverPort = 80;
+        private Thread thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startServerBtn_Click(object sender, EventArgs e)
+        {
+            serverLogsText.Text = "";
+
+            try
+            {
+                httpServer = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    serverPort = int.Parse(serverPortText.Text.ToString());
+
+                    if(serverPort > 65535 || serverPort <= 0)
+                    {
+                       throw new Exception("Серверный порт не попадает в диапазон");
+                    }
+                }
+                catch(Exception ex)
+                {
+                    serverPort = 80;
+                    serverLogsText.Text = "Server failed to start on specified port \n";
+                }
+
+                thread = new Thread(new ThreadStart(this.connectionThreadMethod));
+                thread.Start();
+
+                // Disable and Enable Buttons
+                startServerBtn.Enabled = false;
+                stopServerBtn.Enabled = true;
+
+            }
+            catch(Exception ex)
+            {
+                serverLogsText.Text = "Server starting failed \n";
+            }
+
+            serverLogsText.Text = "Start server";
+        }
+
+        private void stopServerBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Close the Socket
+                httpServer.Close();
+
+                // Kill the Thread
+                thread.Abort();
+
+                // Disable and Enable Buttons
+                startServerBtn.Enabled = true;
+                stopServerBtn.Enabled = false;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Нельзя остановить");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stopServerBtn.Enabled = false;
+        }
+
+        private void connectionThreadMethod()
+        {
+            try
+            {
+                IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, serverPort);
+                httpServer.Bind(endpoint);
+                httpServer.Listen(1);
+                startListeningForConnection();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Нельзя стартовать");
+            }
+        }
+
+        private void startListeningForConnection()
+        {
+            while (true)
+            {
+                DateTime time = DateTime.Now;
+
+                String data = "";
+                byte[] bytes = new byte[2048];
+
+                Socket client = httpServer.Accept();
+                while (true)
+                {
+                    int numBytes = client.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, numBytes);
+
+                    if (data.IndexOf("\r\n") > -1)
+                        break;
+                }
+
+                serverLogsText.Invoke((MethodInvoker)delegate {
+                    serverLogsText.Text += "\r\n\r\n";
+                    serverLogsText.Text += data;
+                    serverLogsText.Text += "\n\n------ End of Request -------";
+                });
+
+                String resHeader = "HTTP/1.1 200 Everything is Fine\nServer: my_csharp_server\nContent-Type: text/html; charset: UTF-8\n\n";
+                String resBody = "<!DOCTYE html> " +
+                    "<html>" +
+                    "<head><title>My Server</title></head>" +
+                    "<body>" +
+                    "<h4>Server Time is: " + time.ToString() + "</h4>" +
+                    "</body></html>";
+
+                String resStr = resHeader + resBody;
+
+                byte[] resData = Encoding.ASCII.GetBytes(resStr);
+
+                client.SendTo(resData, client.RemoteEndPoint);
+
+                client.Close();
+            }
+        }
+    }
+}
